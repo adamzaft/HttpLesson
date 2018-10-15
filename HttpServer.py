37eaaa607ad6bb6ebff0f431f9931e3bf4e28611
adamzaft <43331759+adamzaft@users.noreply.github.com>
2018-10-15 13:52:27 +0300

    Initial commit

---
+++ HttpServer.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+
+class myHTTPRequstHandler(BaseHTTPRequestHandler):
+    # override GET command
+    def do_GET(self):
+        rootdir = 'D:\Python\HttpTests'  # file location
+        try:
+            if self.path.endswith('.html'):
+                f = open(rootdir + self.path)  # opens requested file
+
+                # send 200 response
+                self.send_response(200)
+
+                # send header first
+                self.send_header('Content-type', 'text-html')
+                self.end_headers()
+
+                # send file content to client
+                self.wfile.write(f.read())
+                f.close()
+                return
+        except IOError:
+            self.send_error(404, 'File not found')
+
+def run():
+    print('Starting HttpServer..please hold..')
+
+    # ip and port of server
+    # by default http server port is 80
+    server_address=('127.0.0.1', 80)
+    httpd = HTTPServer(server_address, myHTTPRequstHandler)
+    print('~~~~~~~~~ Server is online - welcome to SKYNET~~~~~~~~~')
+    httpd.serve_forever()
+
+if __name__ =='__main__':
+    run()
+
